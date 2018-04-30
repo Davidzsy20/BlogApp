30d56b6dc387b1107c796171a5948cd54af9db22
David Zou <davidzsy20@gmail.com>
2018-04-30 10:51:05 -0400

    add sanitizer to restful api

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require("express"),
 bodyParser  = require('body-parser'),
 mongoose    = require("mongoose"),
-methodOverride= require("method-override")
+methodOverride= require("method-override"),
+expressSanitizer = require("express-sanitizer");
 
 
 app = express()
@@ -10,6 +11,7 @@ app.set("view engine","ejs")
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.use(expressSanitizer);
 //title 
 
 app.listen(8080, function () {
@@ -44,8 +46,7 @@ app.get("/blogs/new", function(req, res) {
 })
 
 app.post("/blogs", function(req, res) {
-  //creat blog
-  //then, redirect,
+  req.body.blog.body = req.expressSanitizer(req.body.blog.body)
   Blog.create(req.body.blog, function(err, newBlog) {
   if(err) {
       res.render("new");
